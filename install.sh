e7936844f77346e7558753873fceceb904672835
smb00242 <45063457+smb00242@users.noreply.github.com>
2023-10-08 22:15:13 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update package
+opkg update
+
+#install packages
+opkg install iptables iptables-mod-nat-extra redsocks
+
+#Then run this line
+service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/smb00242/SOCKS/raw/main/bdix.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/emonbhuiyan/BDIX-OpenWRT/raw/main/bdix && chmod +x /etc/init.d/bdix
+
+cd /
+clear
+
+
+echo -e "Thanks for installing. Follow me for more updates: SMBiT YouTube Channel"
